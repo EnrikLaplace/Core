bd24eb04ab1b2227f436a248a1f1377f01852a67
EnrikLaplace <matteo.rubini84@gmail.com>
2016-08-19 15:32:06 +0200

    com + lang + util

---
+++ blue/src/blue/util/NetUtils.java
@@ -0,0 +1,123 @@
+package blue.util;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+public class NetUtils {
+	private NetUtils() {}
+	
+	/**
+	 * Calculate random free port emulating system config
+	 * 
+	 * @return
+	 */
+	public static int getRandomPort(){
+		try {
+			ServerSocket sck = new ServerSocket(0);
+			int prt = sck.getLocalPort();
+			sck.close();
+			return prt;
+		} catch (IOException e) {
+			return -1;
+		}
+	}
+
+	public static InetAddress getLocalHost() {
+		try {
+			return InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * Default NET charset is ASCII 7
+	 * @return
+	 */
+	public static Charset defaultCharset(){
+		return Charset.forName("ASCII7");
+	}
+	
+	/**
+	 * Converts string to byte array in ASCII 7
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static byte[] asBytes(String str){
+		return defaultCharset().encode(str).array();
+	}
+	
+	/**
+	 * Converts byte array to string in ASCII 7
+	 * 
+	 * @param arr
+	 * @return
+	 */
+	public static String asBytes(byte[] arr){
+		return defaultCharset().decode(ByteBuffer.wrap(arr)).toString();
+	}
+
+	/**
+	 * Convert URL to address
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static InetSocketAddress toAddress(URL url) {
+		return new InetSocketAddress(url.getHost(), url.getPort() == -1? url.getDefaultPort():url.getPort());
+	}
+
+	public static InetSocketAddress getLocalAddress(int port) {
+		return new InetSocketAddress(getLocalHost(), port);
+	}
+	
+	public static byte[] resolve(String host){
+		try {
+			return InetAddress.getByName(host).getAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static String[] getAvailableAddresses(){
+		try {
+			Enumeration<NetworkInterface> ielist;
+			ArrayList<String> ret = new ArrayList<String>();
+			ielist = NetworkInterface.getNetworkInterfaces();
+			while(ielist.hasMoreElements()) {
+				NetworkInterface netint = ielist.nextElement();
+				Enumeration<InetAddress> ee = netint.getInetAddresses();
+			    while (ee.hasMoreElements())
+			    {
+			        InetAddress i = (InetAddress) ee.nextElement();
+			        ret.add(i.getCanonicalHostName());
+			    }
+			}
+			return ret.toArray(new String[ret.size()]);
+		} catch (SocketException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		for(String s:getAvailableAddresses()) {
+			System.out.println(s);
+		}
+	}
+}
